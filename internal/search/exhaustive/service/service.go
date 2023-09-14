77c5a13ac6fd4e81ec9be15aca21108e8845a7d2
Stefan Hengl <stefan@sourcegraph.com>
2023-09-14 15:56:49 +0200

    search jobs: fix pagination
    - add state CANCELED
    - fix ORDER_by
    - fix filtering by state and query
    - fix general pagination
    
    Test case:
    manual testing via API Console

--- internal/search/exhaustive/service/service.go
+++ internal/search/exhaustive/service/service.go
@@ -137,7 +137,7 @@ func (s *Service) GetSearchJob(ctx context.Context, id int64) (_ *types.Exhausti
 	return s.store.GetExhaustiveSearchJob(ctx, id)
 }
 
-func (s *Service) ListSearchJobs(ctx context.Context) (jobs []*types.ExhaustiveSearchJob, err error) {
+func (s *Service) ListSearchJobs(ctx context.Context, args store.ListArgs) (jobs []*types.ExhaustiveSearchJob, err error) {
 	ctx, _, endObservation := s.operations.listSearchJobs.With(ctx, &err, observation.Args{})
 	defer func() {
 		endObservation(1, opAttrs(
@@ -145,5 +145,5 @@ func (s *Service) ListSearchJobs(ctx context.Context) (jobs []*types.ExhaustiveS
 		))
 	}()
 
-	return s.store.ListExhaustiveSearchJobs(ctx)
+	return s.store.ListExhaustiveSearchJobs(ctx, args)
 }
